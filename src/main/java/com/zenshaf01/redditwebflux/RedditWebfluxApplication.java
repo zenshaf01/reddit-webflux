54909e627ddbe005d71905efa95ff05b0d97433d
starlord <zeeshanshafqat1@outlook.com>
2026-04-06 06:44:31 -0400

    initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/zenshaf01/redditwebflux/RedditWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.zenshaf01.redditwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zenshaf01/redditwebflux/RedditWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenshaf01.redditwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
